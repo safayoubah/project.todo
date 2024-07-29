1b1e07a486140cf7bd975114b3c4cbccf2b8fcb9
safayoubah <safayoubah2024@gmail.com>
2024-07-29 17:36:27 +0300

    eleve js

--- eleve.js
+++ eleve.js
@@ -1,6 +1,11 @@
 const cherche = document.getElementsByClassName('cherche');
 const inputCherche = document.getElementById('input-cherche');
 const chercheList = document.getElementById('cherche-list');
+const eleveAdd = document.getElementById('eleve-add');
+const  plus = document.getElementById('plus');
+const close = document.getElementById('close');
+const ajouteButton = document.getElementById('ajoute-button');
+const ajouteInput = document.getElementById('ajoute-input');
 
 const students = ["aly", "safayou", "moustapha", "muhamed", "addouraman", "tariq", "ashraf", "amadou"];
 
@@ -21,3 +26,20 @@ function filterList() {
     displayList(filteredStudents);
 }
 displayList(students);
+
+plus.addEventListener("click", () => {
+    eleveAdd.style.display = "block"
+})
+
+close.addEventListener("click", () => {
+    eleveAdd.style.display = "none"
+})
+
+ajouteButton.addEventListener("click", () => {
+   const newStudent = ajouteInput.value
+   students.push(newStudent) 
+   displayList(students);
+   eleveAdd.style.display = "none"
+})
+
+
